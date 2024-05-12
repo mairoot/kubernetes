eb25f34c76546729a9eb05bb86fc8d91c6802bff
mairoot <maitoor@163.com>
2024-05-12 07:12:59 -0400

    +

--- mall-swarm/base-svc/run.sh
+++ mall-swarm/base-svc/run.sh
@@ -20,19 +20,46 @@ do
     kubectl label nodes $node_i app=nacos
     kubectl label nodes $node_i minio=minio
 done
+sleep 30
+}
+
+svc_c(){
+for i in `find nfs -type f \( -name *.yaml -o -name *.yml \)`
+do
+  if [[ $i =~ \.yaml$ || $i =~ \.yml$ ]];then                                                                                     
+    kubectl apply -f ./$i                                                                                                         
+  fi                                                                                                                              
+done                                                                                                                              
+sleep 30
 }
 
 
+prom_c(){                                                                                                                         
+  cd prometheus/manifests/                                                                                                        
+  kubectl delete -f  grafana-networkPolicy.yaml                                                                                   
+  kubectl apply --server-side -f setup                                                                                            
+  kubectl wait --for condition=Established --all CustomResourceDefinition --namespace=monitoring                                  
+  kubectl apply -f .                                                                                                              
+  kubectl delete -f prometheus-networkPolicy.yaml                                                                                 
+  kubectl delete -f alertmanager-networkPolicy.yaml                                                                               
+  kubectl delete -f grafana-networkPolicy.yaml                                                                                    
+  cd ../../                                                                                                                       
+sleep 30
+} 
 
-yaml_c(){
-for i in `find . -type f`
+
+svc_c(){
+for i in `find . -type d \( -path ./prometheus -o -path ./nfs \) -prune -o -type f \( -name *.yaml -o -name *.yml \) -print`
 do
   if [[ $i =~ \.yaml$ || $i =~ \.yml$ ]];then
     kubectl apply -f ./$i
   fi
 done
+sleep 30
 }
 
 
 sys_c
-yaml_c
+nfs_c
+prom_c
+svc_c
